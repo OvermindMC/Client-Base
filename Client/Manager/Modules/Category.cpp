e1d356133f21c87d66b50cec2816e20ba10e1009
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-24 14:01:49 +0100

    Update Category.cpp

--- Client/Manager/Modules/Category.cpp
+++ Client/Manager/Modules/Category.cpp
@@ -1,10 +1,6 @@
 #include "Category.h"
 #include "Module/Module.h"
 
-void Category::registerMod(std::unique_ptr<Module> mod) {
-    this->modules.emplace_back(std::move(mod));
-};
-
 std::vector<Module*> Category::getModules() const {
     std::vector<Module*> list;
 
