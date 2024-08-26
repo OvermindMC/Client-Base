f057a6c537ccd38dabf0cc6a73ee7b6c3d1c37cb
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-26 09:58:21 +0100

    Update TestModule.cpp

--- Client/Manager/Modules/Module/Other/TestModule.cpp
+++ Client/Manager/Modules/Module/Other/TestModule.cpp
@@ -5,31 +5,31 @@ TestMod::TestMod(Category* c) : Module(c) {
 
     this->registerEvent<EventBase::Type::onTick, EventBase::Priority::High>(
         [&]() {
-            //
+            //Debugger::log("OnTick");
         }
     );
 
     this->registerEvent<EventBase::Type::onEnable, EventBase::Priority::High>(
         [&]() {
-            Debugger::log("OnEnable");
+            //Debugger::log("OnEnable");
         }
     );
 
     this->registerEvent<EventBase::Type::onDisable, EventBase::Priority::High>(
         [&]() {
-            Debugger::log("OnDisable");
+            //Debugger::log("OnDisable");
         }
     );
 
     this->registerEvent<EventBase::Type::onLevel, EventBase::Priority::High>(
         [&]() {
-            Debugger::log("OnLevel");
+            //Debugger::log("OnLevel");
         }
     );
 
     this->registerEvent<EventBase::Type::onRender, EventBase::Priority::High>(
         [&]() {
-            Debugger::log("OnRender");
+            //Debugger::log("OnRender");
         }
     );
 };
