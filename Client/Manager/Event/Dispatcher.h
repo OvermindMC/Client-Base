893979eb44664df975da9718ecd6232d3b44b9df
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-26 09:58:19 +0100

    Update Dispatcher.h

--- Client/Manager/Event/Dispatcher.h
+++ Client/Manager/Event/Dispatcher.h
@@ -12,14 +12,41 @@ public:
 
     template<EventBase::Type T, EventBase::Priority P, typename... Args>
     void registerEvent(std::function<void(Args...)> callback) {
-        std::unique_ptr<Event<T, P, Args...>> event = std::make_unique<Event<T, P, Args...>>(callback);
-        this->events.at(T).emplace_back(std::move(event));
+        auto event = std::make_unique<Event<T, P, Args...>>(callback);
+        this->events[T].emplace_back(std::move(event));
+    };
+
+    template<EventBase::Type T, EventBase::Priority P>
+    void registerEvent(std::function<void()> callback) {
+        auto event = std::make_unique<Event<T, P>>(callback);
+        this->events[T].emplace_back(std::move(event));
     };
 
     template<typename EventBase::Type T>
     void unregisterEvents() {
         auto iter = events.at(T);
         iter.clear();
+    };
+
+    template<EventBase::Type T, typename... Args>
+    void dispatchEvent(Args... args) {
+        auto list = this->getEvents<T>();
+
+        std::sort(list.begin(), list.end(), [](EventBase* a, EventBase* b) {
+            return a->getPriority() > b->getPriority();
+        });
+
+        for (auto& event : list) {
+            if (auto specificEvent = dynamic_cast<Event<T, EventBase::Priority::Low, Args...>*>(event)) {
+                specificEvent->call(std::forward<Args>(args)...);
+            }
+            else if (auto specificEvent = dynamic_cast<Event<T, EventBase::Priority::Medium, Args...>*>(event)) {
+                specificEvent->call(std::forward<Args>(args)...);
+            }
+            else if (auto specificEvent = dynamic_cast<Event<T, EventBase::Priority::High, Args...>*>(event)) {
+                specificEvent->call(std::forward<Args>(args)...);
+            };
+        };
     };
 
     template<EventBase::Type T>
