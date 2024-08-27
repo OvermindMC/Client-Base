a92084783e3fef8c81c16dad3cba9ee6d37cbef4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-27 20:36:16 +0100

    Update DXGI_Present.h

--- Client/Manager/Sigs/Subs/DXGI_Present.h
+++ Client/Manager/Sigs/Subs/DXGI_Present.h
@@ -2,9 +2,9 @@
 
 #include "../Sig.h"
 
-class DXGIPresent_Sig : public Sig<void**> {
+class DXGIPresent_Sig : public Sig<void*> {
 public:
-    DXGIPresent_Sig() : Sig<void**>("DXGIPresent_Func", "48 89 5C 24 10 48 89 74 24 20 55 57 41 56", BaseSig::Type::Function, "dxgi.dll") {
+    DXGIPresent_Sig() : Sig<void*>("DXGIPresent_Func", "48 89 5C 24 10 48 89 74 24 20 55 57 41 56", BaseSig::Type::Function, "dxgi.dll") {
         //
     };
 };
