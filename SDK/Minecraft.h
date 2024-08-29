c4feee67c4bc65195c441e2e4485be00900e69cc
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-29 15:15:41 +0100

    Update Minecraft.h

--- SDK/Minecraft.h
+++ SDK/Minecraft.h
@@ -6,4 +6,14 @@ class MC {
 public:
     static ClientInstance* getClientInstance();
     static Player* getPlayer();
+
+    static std::vector<Actor*> getEntities();
+};
+
+class MCUtils {
+public:
+    static bool isHostile(uint8_t);
+    static bool isPassive(uint8_t);
+
+    static void forEach(std::function<void(Actor*, bool&)> callback, std::function<bool(Actor*, Actor*)> sort = nullptr, std::function<bool(Actor*)> includeOnly = nullptr);
 };
