2da21a2fbc48dbc11966887b03a563b101c683cd
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-30 19:02:46 +0100

    Update ClientInstance.cpp

--- SDK/Subs/ClientInstance.cpp
+++ SDK/Subs/ClientInstance.cpp
@@ -3,3 +3,10 @@
 Player* ClientInstance::getPlayer() {
     return Mem::CallVFunc<Player*>(this, 29);
 };
+
+std::string& ClientInstance::getTopScreenName() {
+    static std::string out;
+    Mem::CallVFunc<void, std::string*>(this, 147, &out);
+
+    return out;
+};
