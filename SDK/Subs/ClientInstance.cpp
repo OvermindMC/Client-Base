62216719317341087b569f2bd04b2c802b0f3b13
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-31 23:17:31 +0100

    Update ClientInstance.cpp

--- SDK/Subs/ClientInstance.cpp
+++ SDK/Subs/ClientInstance.cpp
@@ -8,6 +8,14 @@ GuiData* ClientInstance::getGuiData() {
     return *(GuiData**)((uintptr_t)(this) + 0x558);
 };
 
+void ClientInstance::releaseMouse() {
+    return Mem::CallVFunc<void>(this, 334);
+};
+
+void ClientInstance::grabMouse() {
+    return Mem::CallVFunc<void>(this, 333);
+};
+
 std::string& ClientInstance::getTopScreenName() {
     static std::string out;
     Mem::CallVFunc<void, std::string*>(this, 147, &out);
