cfeaf5c8965f73fd09900fd250693e01b00e3fa4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-31 23:26:14 +0100

    Update ClickGui.h

--- Client/Manager/Modules/Module/Render/ClickGui.h
+++ Client/Manager/Modules/Module/Render/ClickGui.h
@@ -5,8 +5,10 @@
 class ClickGui : public Module {
 public:
     ClickGui(Category*);
+    ~ClickGui() override;
 
     std::string getName() const override;
+    void revert();
 
     std::pair<float, float> blurProg = { 0.f, 0.f };
 };
