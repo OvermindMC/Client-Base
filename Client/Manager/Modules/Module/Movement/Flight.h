2e1c88f91763aa69ec63bc7cd28087081c9208c9
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-09-04 04:43:55 +0100

    Update Flight.h

--- Client/Manager/Modules/Module/Movement/Flight.h
+++ Client/Manager/Modules/Module/Movement/Flight.h
@@ -5,8 +5,13 @@
 class Flight : public Module {
 public:
     Flight(Category*);
+    ~Flight() override;
 
     std::string getName() const override;
 
+    void revert();
+
     float flySpeed = 0.08f;
+    bool canModify = false;
+    std::pair<bool, bool> prevStates = { false, false };
 };
