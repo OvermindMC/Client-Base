3b0338692eed547e98941e9f2e816ceee8a56f24
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-09-06 23:52:48 +0100

    Update Level_Tick.h

--- Client/Manager/Hooks/Subs/Level_Tick.h
+++ Client/Manager/Hooks/Subs/Level_Tick.h
@@ -9,9 +9,9 @@ public:
     LevelTick_Hook(Manager* mgr) : Hook<void, void*>(mgr, "Level_Tick", mgr->getSig<Level_Sig, void**>()[99],
     [&](void* level) -> void {
         Manager* mgr = this->getMgr();
-        Player* player = MC::getPlayer();
 
         if(mgr && mgr->isTicking()) {
+            Player* player = MC::getPlayer();
             mgr->dispatchEvent<LevelEvent>(
                 {
                     player
