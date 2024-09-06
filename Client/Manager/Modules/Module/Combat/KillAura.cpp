85aff81fb2ba14449de96595129f1f59b1856af8
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-09-07 00:19:32 +0100

    Update KillAura.cpp

--- Client/Manager/Modules/Module/Combat/KillAura.cpp
+++ Client/Manager/Modules/Module/Combat/KillAura.cpp
@@ -5,19 +5,28 @@ Killaura::Killaura(Category* c) : Module(c) {
 
     this->registerEvent<LevelEvent, EventPriority::High>(
         [&](const LevelEvent& ev) {
-            if(!ev.mPlayer)
+            static float elapsedTime, tickInterval = (this->ticksDelay / 10.f);
+
+            float deltaTime = ImGui::GetIO().DeltaTime;
+            elapsedTime += deltaTime;
+
+            if(!ev.mPlayer || elapsedTime < tickInterval)
                 return;
+            
+            elapsedTime = 0.f;
             
             Player* player = ev.mPlayer;
             Vec3 myPos = player->getPosition();
             GameMode* GM = player->getGameMode();
             uint64_t lRuntimeId = player->getRuntimeId();
 
+            int i = 0;
             MCUtils::forEach(
                 [&](Actor* entity, bool& continueLoop) {
+                    continueLoop = i < 5;
                     player->swing();
                     GM->attack(entity);
-                    continueLoop = false;
+                    ++i;
                 }, [&](Actor* a, Actor* b) -> bool {
                     return a->getPosition().dist(myPos) < b->getPosition().dist(myPos);
                 }, [&](Actor* entity) -> bool {
